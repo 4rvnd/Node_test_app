261db665e70469a44a0756cc329aac42f2bc2589
Aravind <aravind17mahadevan@gmail.com>
2018-10-03 21:56:23 +0530

    my first commit

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const app = express();
 
+app.use(cors());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(morgan('dev'));
+
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(morgan('dev'));
+
 
 
 app.get('/', (req, res, next) => 
-	{
-		res.send(req.headers.one);
-		next();
-	}
-
-);
-
-app.post('/', (req, res, next) => 
-	{
-		res.send(req.headers.one);
-		next();
+	{	console.log(res.headers.one);
+	
+		// res.send(req.headers.one);
+		// next();
+		// res.send(req.headers.qwe);
+		// next();
 	}
 );
+
+
+	// app.post('/', (req, res, next) => 
+	// 	{
+	// 		res.send(req.headers.one + req.headers.qwe);
+	// 		next();
+	// 	}
+
+	// 	);
+
+
+
+// app.post('/', (req, res, next) => 
+// 	{
+// 		if(req.headers.one == '11'){
+// 			res.send(req.headers.one);
+// 		}
+// 		else{
+// 			res.send("Bad");
+// 		next();
+// 	}
+// }
+// );
 
 app.listen(8080, (err) =>
 	{
